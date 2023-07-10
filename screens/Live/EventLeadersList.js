b7a1aa94b53e0769a1c3f746cdfc688cfe2b29c6
Kyle Craft <craftjk@gmail.com>
2023-07-10 01:48:50 -0600

    finished expandedRoundSummary

--- screens/Live/EventLeadersList.js
+++ screens/Live/EventLeadersList.js
@@ -15,6 +15,83 @@ import Colors from "../../constants/Colors";
 import { Ionicons } from "@expo/vector-icons";
 import round1Results from "../../sampleData/round1Results";
 import round2Results from "../../sampleData/round2Results";
+import fetchPlayerAliSmith from "../../sampleData/fetchPlayerAliSmith";
+import fetchPlayerGannonBuhr from "../../sampleData/fetchPlayerGannonBuhr";
+
+function makeExpandedRow(data, imgSrc) {
+  let playerData = fetchPlayerAliSmith.data;
+  if (data.Name === "Gannon Buhr") {
+    playerData = fetchPlayerGannonBuhr.data;
+  }
+
+  // kc_todo replace flag with an Image
+  return (
+    <View style={styles.expandedRowContainer}>
+      <View style={styles.expandedPlayerInfoContainer}>
+        <Image style={styles.expandedPlayerPic} source={imgSrc} />
+        <View style={styles.expandedPlayerInfo}>
+          <View style={styles.playerPicMask} />
+          <Text style={styles.expandedPlayerInfoNameText}>
+            {playerData.Name}
+          </Text>
+          <Text style={styles.expandedPlayerInfoLocationText}>
+            {playerData.Location}
+          </Text>
+          <View style={styles.expandedPlayerPdgaInfoContainer}>
+            <View style={styles.expandedFlagIconContainer}>
+              <Text style={styles.expandedFlagText}>🇺🇸</Text>
+            </View>
+            <Text
+              style={styles.expandedPlayerPdgaInfoText}
+            >{`#${playerData.PDGANum}`}</Text>
+            <View style={styles.ratingDiamond}>
+              <Text style={styles.ratingText}>R</Text>
+            </View>
+            <Text
+              style={styles.expandedPlayerPdgaInfoText}
+            >{`${playerData.Rating}`}</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.expandedRoundSummaryContainer}>
+        <View style={styles.expandedRoundSummarySubcontainer}>
+          <Text style={styles.expandedRoundSummarySubheaderText}>Total</Text>
+          <Text style={styles.expandedRoundSummaryTotalText}>-28</Text>
+        </View>
+        <View style={{ width: 1, backgroundColor: Colors.grayscale[2] }} />
+        <View style={styles.expandedRoundSummarySubcontainer}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <View style={styles.smallRatingDiamond}>
+              <Text style={styles.smallRatingText}>R</Text>
+            </View>
+            <Text style={styles.expandedRoundSummarySubheaderText}>Event</Text>
+          </View>
+          <Text style={styles.expandedRoundSummaryTotalText}>
+            {playerData.AverageRoundRating}
+            {playerData.RatingDiff > 0 ? (
+              <Text
+                style={{
+                  ...styles.expandedRoundSummaryTotalText,
+                  color: "lightgreen",
+                }}
+              >{` ^${playerData.RatingDiff}`}</Text>
+            ) : null}
+          </Text>
+        </View>
+      </View>
+      <View style={styles.expandedRoundNavContainer}></View>
+      <View style={styles.expandedRoundHeaderContainer}></View>
+      <View style={styles.expandedHoleDetailsContainer}></View>
+      <View style={styles.expandedCourseMetadataContainer}></View>
+    </View>
+  );
+}
 
 function makeHeaderRow(data) {
   return (
@@ -36,59 +113,72 @@ function makeHeaderRow(data) {
 }
 
 function makeScoreRow(data, expandedRows, setExpandedRows) {
-  let dataSource = require("../../sampleData/placeholder-person2.jpeg");
+  let imgSrc = require("../../sampleData/placeholder-person2.jpeg");
 
   if (data.Name === "Gannon Buhr") {
-    dataSource = require("../../sampleData/Gannon-Buhr.jpeg");
+    imgSrc = require("../../sampleData/Gannon-Buhr.jpeg");
   } else if (data.Name === "Ali Smith") {
-    dataSource = require("../../sampleData/ali_smith_thumbnail.jpeg");
+    imgSrc = require("../../sampleData/ali_smith_thumbnail.jpeg");
   } else if (data.Name === "Joseph Anderson") {
-    dataSource = require("../../sampleData/joseph-anderson.jpeg");
+    imgSrc = require("../../sampleData/joseph-anderson.jpeg");
   } else if (data.Name === "Sullivan Tipton") {
-    dataSource = require("../../sampleData/sullivan-t.jpeg");
+    imgSrc = require("../../sampleData/sullivan-t.jpeg");
   } else if (data.Name === "Holyn Handley") {
-    dataSource = require("../../sampleData/holyn.jpeg");
+    imgSrc = require("../../sampleData/holyn.jpeg");
   } else if (data.Name === "Hanna Huynh") {
-    dataSource = require("../../sampleData/hanna-huynh.jpeg");
+    imgSrc = require("../../sampleData/hanna-huynh.jpeg");
   } else if (data.Name === "Lisa Fajkus") {
-    dataSource = require("../../sampleData/lisa-fajkus.jpeg");
+    imgSrc = require("../../sampleData/lisa-fajkus.jpeg");
   }
 
   return (
-    <View style={styles.rowContainer}>
-      <View style={styles.rowPlaceContainer}>
-        <Text style={styles.rowPlaceText}>{data.RunningPlace}</Text>
-      </View>
-      <View style={styles.rowPicContainer}>
-        <Image style={styles.playerThumbnail} source={dataSource} />
-        <View style={styles.flagIconContainer}>
-          <Text>🇺🇸</Text>
+    <View key={data.ResultID}>
+      <Pressable
+        onPress={() => {
+          setExpandedRows({
+            ...expandedRows,
+            [data.ResultID]: !expandedRows[data.ResultID],
+          });
+        }}
+        style={styles.rowContainer}
+      >
+        <View style={styles.rowPlaceContainer}>
+          <Text style={styles.rowPlaceText}>{`${data.Tied ? "T" : ""}${
+            data.RunningPlace
+          }`}</Text>
         </View>
-      </View>
-      <View style={styles.rowPlayerDataContainer}>
-        <Text style={styles.rowPlayerNameText}>{data.Name}</Text>
-      </View>
-      <View style={styles.rowScoreContainer}>
-        <Text style={styles.rowText}>
-          {data.ParThruRound < 0
-            ? data.ParThruRound
-            : data.ParThruRound === 0
-            ? "E"
-            : "+" + data.ParThruRound}
-        </Text>
-      </View>
-      <View style={styles.rowScoreContainer}>
-        <Text style={styles.rowText}>
-          {data.RoundtoPar < 0
-            ? data.RoundtoPar
-            : data.RoundtoPar === 0
-            ? "E"
-            : "+" + data.RoundtoPar}
-        </Text>
-      </View>
-      <View style={styles.rowScoreContainer}>
-        <Text style={styles.rowThruText}>F</Text>
-      </View>
+        <View style={styles.rowPicContainer}>
+          <Image style={styles.playerThumbnail} source={imgSrc} />
+          <View style={styles.flagIconContainer}>
+            <Text>🇺🇸</Text>
+          </View>
+        </View>
+        <View style={styles.rowPlayerDataContainer}>
+          <Text style={styles.rowPlayerNameText}>{data.Name}</Text>
+        </View>
+        <View style={styles.rowScoreContainer}>
+          <Text style={styles.rowText}>
+            {data.ParThruRound < 0
+              ? data.ParThruRound
+              : data.ParThruRound === 0
+              ? "E"
+              : "+" + data.ParThruRound}
+          </Text>
+        </View>
+        <View style={styles.rowScoreContainer}>
+          <Text style={styles.rowText}>
+            {data.RoundtoPar < 0
+              ? data.RoundtoPar
+              : data.RoundtoPar === 0
+              ? "E"
+              : "+" + data.RoundtoPar}
+          </Text>
+        </View>
+        <View style={styles.rowScoreContainer}>
+          <Text style={styles.rowThruText}>F</Text>
+        </View>
+      </Pressable>
+      {expandedRows[data.ResultID] ? makeExpandedRow(data, imgSrc) : null}
     </View>
   );
 }
@@ -147,7 +237,6 @@ const styles = StyleSheet.create({
     marginHorizontal: moderateScale(10),
     marginVertical: moderateScale(4),
     borderRadius: moderateScale(7),
-    height: moderateScale(70),
     backgroundColor: Colors.white,
   },
   listContainer: {
@@ -170,8 +259,8 @@ const styles = StyleSheet.create({
   },
   flagIconContainer: {
     position: "absolute",
-    bottom: moderateScale(6),
-    right: moderateScale(6),
+    bottom: moderateScale(7),
+    right: moderateScale(7),
     backgroundColor: "white",
     borderRadius: moderateScale(10),
     height: moderateScale(15),
@@ -188,6 +277,7 @@ const styles = StyleSheet.create({
     width: moderateScale(25),
     justifyContent: "center",
     alignItems: "center",
+    height: moderateScale(70),
   },
   rowPlaceText: {
     fontWeight: "bold",
@@ -240,6 +330,137 @@ const styles = StyleSheet.create({
     height: moderateScale(45),
     width: moderateScale(45),
     borderRadius: moderateScale(30),
+  },
+
+  // expanded row styles below
+  expandedRowContainer: {
+    marginHorizontal: moderateScale(10),
+    marginTop: moderateScale(-8),
+    borderBottomLeftRadius: moderateScale(7),
+    borderBottomRightRadius: moderateScale(7),
+  },
+  expandedPlayerInfoContainer: {
+    flexDirection: "row",
+    overflow: "hidden",
+  },
+  expandedPlayerPic: {
+    height: moderateScale(120),
+    width: moderateScale(100),
+    borderBottomLeftRadius: moderateScale(7),
+  },
+  expandedPlayerInfoNameText: {
+    color: Colors.white,
+    marginTop: moderateScale(3),
+    fontSize: moderateScale(21),
+  },
+  expandedPlayerInfoLocationText: {
+    color: Colors.grayscale[4],
+    fontSize: moderateScale(12),
+  },
+  expandedPlayerInfo: {
+    backgroundColor: Colors.grayscale[1],
+    flex: 1,
+    padding: moderateScale(15),
+  },
+  expandedPlayerPdgaInfoContainer: {
+    marginTop: moderateScale(5),
+    backgroundColor: Colors.grayscale[2],
+    padding: moderateScale(8),
+    flexDirection: "row",
+    borderRadius: moderateScale(20),
+    alignItems: "center",
+    justifyContent: "center",
+    alignSelf: "flex-start",
+  },
+  expandedFlagIconContainer: {
+    borderRadius: moderateScale(10),
+    height: moderateScale(15),
+    width: moderateScale(15),
+    overflow: "hidden",
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: moderateScale(6),
+  },
+  expandedFlagText: {
+    fontSize: moderateScale(60),
+    marginTop: moderateScale(-30),
+    marginLeft: moderateScale(-20),
+  },
+  expandedPlayerPdgaInfoText: {
+    marginLeft: moderateScale(3),
+    color: Colors.white,
+    fontWeight: "bold",
+    fontSize: moderateScale(12),
+  },
+  expandedRoundSummaryContainer: {
+    flexDirection: "row",
+    marginTop: -1,
+    height: moderateScale(42),
+  },
+  expandedRoundSummarySubcontainer: {
+    backgroundColor: Colors.black,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  expandedRoundSummarySubheaderText: {
+    color: Colors.grayscale[3],
+    fontWeight: "bold",
+    fontSize: moderateScale(11),
+  },
+  expandedRoundSummaryTotalText: {
+    color: Colors.white,
+    fontWeight: "bold",
+    fontSize: moderateScale(12),
+  },
+  expandedRoundNavContainer: {
+    flexDirection: "row",
+  },
+  expandedRoundHeaderContainer: {
+    flexDirection: "row",
+  },
+  expandedHoleDetailsContainer: {
+    flexDirection: "row",
+  },
+  expandedCourseMetadataContainer: {
+    flexDirection: "row",
+  },
+  ratingDiamond: {
+    marginLeft: moderateScale(8),
+    backgroundColor: Colors.white,
+    height: moderateScale(9),
+    width: moderateScale(9),
+    alignItems: "center",
+    justifyContent: "center",
+    transform: [{ rotate: "45deg" }],
+  },
+  ratingText: {
+    fontSize: 8,
+    fontWeight: "bold",
+    transform: [{ rotate: "-45deg" }],
+  },
+  smallRatingDiamond: {
+    marginRight: moderateScale(3),
+    backgroundColor: Colors.grayscale[6],
+    height: moderateScale(6),
+    width: moderateScale(6),
+    alignItems: "center",
+    justifyContent: "center",
+    transform: [{ rotate: "45deg" }],
+  },
+  smallRatingText: {
+    fontSize: 5,
+    fontWeight: "bold",
+    transform: [{ rotate: "-45deg" }],
+  },
+  playerPicMask: {
+    backgroundColor: Colors.grayscale[1],
+    width: moderateScale(30),
+    left: moderateScale(0),
+    top: moderateScale(-20),
+    height: moderateScale(300),
+    position: "absolute",
+    transform: [{ rotate: "-6deg" }],
   },
 });
 
